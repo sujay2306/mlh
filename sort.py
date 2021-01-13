eead35ad65432609463b07546ff4481c804dbce6
Sujay ks <sujayks007@gmail.com>
2021-01-13 07:27:04 +0530

    python program to sort alphabets

--- sort.py
+++ sort.py
@@ -1,7 +1,18 @@
+# Program to sort alphabetically the words form a string provided by the user
 
-numbers = [1, 3, 4, 2] 
-  
-# Sorting list of Integers in ascending 
-numbers.sort() 
-  
-print(numbers) 
+my_str = "Hello this Is an Example With cased letters"
+
+# To take input from the user
+#my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+   print(word)
